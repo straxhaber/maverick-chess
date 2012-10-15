6ac8faa940c93820ef4dca7c40c1a26ca5308d90
Matthew Strax-Haber <web+github@straxhaber.com>
2012-10-14 20:36:33 -0400

    Added stub and spec for core

---
+++ core.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import os,sys
+
+
+class Game:
+    """Represents a chess game in Maverick"""
+
+    MOVE_SUCCESS = 34923498
+
+
+def main():
+    print "This class should not be run directly"
+
+if __name__ == '__main__':
+    main()
