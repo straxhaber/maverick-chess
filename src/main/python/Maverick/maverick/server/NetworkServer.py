a56512e081285dcd91ed77c2a0dc1d79af9a7060
Matthew Strax-Haber <web+git@straxhaber.com>
2012-10-25 15:38:43 -0400

    Re-factored network server switch for various requests

--- src/main/python/Maverick/maverick/server/NetworkServer.py
+++ src/main/python/Maverick/maverick/server/NetworkServer.py
@@ -26,16 +26,15 @@ class MaverickProtocol(basicProtocols.LineOnlyReceiver):
     _version = "1.0a1"
 
     # put a TournamentSystem instance here
-    __tournSys = None
+    _ts = None
 
     def __init__(self, tournamentSystem):
         """Store a reference to the TournamentSystem backing up this server"""
-        MaverickProtocol.__tournSys = tournamentSystem
+        MaverickProtocol._ts = tournamentSystem
 
     def connectionMade(self):
         """When a client connects, provide a welcome message"""
-
-        ## TODO: log client connections
+        ## TODO: log connections
 
         # Print out the server name and version
         #  (e.g., "MaverickChessServer/1.0a1")
@@ -46,131 +45,61 @@ class MaverickProtocol(basicProtocols.LineOnlyReceiver):
 
     def connectionLost(self, reason=None):
         """When a client disconnects, log it"""
-
-        ## TODO: log client disconnections
-        
+        ## TODO: log disconnections
+    
     def lineReceived(self, line):
         """Take input line-by-line and redirect it to the core"""
 
-        ## TODO: log client requests
-        ## FIXME: finish writing this method
-        ## TODO: Sanitize input
-        ## FIXME: actually parse request
-        requestName = line.split(" ")[0]
-        reqArgs = {"name" : "Matthew Strax-Haber"}
-
-        errorString = None
+        ## TODO: JSON-ify
+        requestName = line.split(" ")[0] # Name of request (e.g., "REGISTER")
+        requestArgs = {"name" : "Matthew Strax-Haber"} ## FIXME: parse arguments
+        ## TODO: log requests
         
-        ## TODO: re-factor out common code to shorten and simplify
-
-        if requestName == "REGISTER":
-            expectedArguments = {"name"}
-            if expectedArguments != set(reqArgs):
+        # Map of valid request names to
+        #  - corresponding TournamentSystem function
+        #  - expected arguments
+        #  - expected return values (currently unused)
+        validRequests = {"REGISTER": (self._ts.register,
+                                      {"name"},
+                                      {"playerID"}),
+                         "JOIN_GAME": (self._ts.joinGame,
+                                       {"playerID"},
+                                       {"gameID"}),
+                         "GET_STATUS": (self._ts.getStatus,
+                                        {"playerID", "gameID"},
+                                        {"status"}),
+                         "GET_STATE": (self._ts.getState,
+                                       {"playerID", "gameID"},
+                                       {"youAre", "turn", "board", "history"}),
+                         "MAKE_PLY": (self._ts.makePly,
+                                      {"playerID", "gameID",
+                                       "fromRank", "fromFile",
+                                       "toRank", "toFile"},
+                                      {"status"})}
+        
+        errMsg = None # If this gets set, there was an error
+        if requestName in validRequests.keys():
+            (tsCommand, expArgs, _) = validRequests[requestName]
+            if expArgs != set(requestArgs.keys()):
                 fStr = "Invalid arguments, expected: {0}"
-                errorString = fStr.format(str(list(expectedArguments)))
+                errMsg = fStr.format(str(list(expArgs)))
             else:
-                (successP, result) = self.__tournSys.register(reqArgs["name"])
+                (successP, result) = tsCommand(**requestArgs)
                 if successP:
-                    fStr = "SUCCESS \"{0}\""
-                    response = fStr.format(str(result["playerID"]))
-                else:
-                    errorString = result["error"]
-
-        elif requestName == "PLAY_GAME":
-            expectedArguments = {"playerID"}
-            if expectedArguments != set(reqArgs):
-                fStr = "Invalid arguments, expected: {0}"
-                errorString = fStr.format(str(list(expectedArguments)))
-            else:
-                playerID = reqArgs["playerID"]
-                (successP, result) = self.__tournSys.playGame(playerID) ## FIXME
-                if successP:
-                    fStr = "SUCCESS \"{0}\""
-                    response = fStr.format(str(result["gameID"]))
-                else:
-                    errorString = result["error"]
-
-        elif requestName == "GET_STATUS":
-            expectedArguments = {"playerID", "gameID"}
-            if expectedArguments != set(reqArgs):
-                fStr = "Invalid arguments, expected: {0}"
-                errorString = fStr.format(str(list(expectedArguments)))
-            else:
-                playerID = reqArgs["playerID"]
-                gameID = reqArgs["gameID"]
-                (successP, result) = self.__tournSys.getStatus(
-                    playerID, gameID) ## FIXME
-                if successP:
-                    fStr = "SUCCESS \"{0}\""
-                    response = fStr.format() ## FIXME
-                else:
-                    errorString = result["error"]
-
-        elif requestName == "GET_STATE":
-            expectedArguments = {"playerID", "gameID"}
-            if expectedArguments != set(reqArgs):
-                fStr = "Invalid arguments, expected: {0}"
-                errorString = fStr.format(str(list(expectedArguments)))
-            else:
-                playerID = reqArgs["playerID"]
-                gameID = reqArgs["gameID"]
-                (successP, result) = self.__tournSys.getState(playerID, gameID)
-                if successP:
-                    fStr = "SUCCESS \"{0}\""
-                    response = fStr.format() ## FIXME
-                else:
-                    errorString = result["error"]
-
-        elif requestName == "MAKE_PLY":
-            expectedArguments = {"playerID", "gameID",
-                               "fromRank", "fromFile",
-                               "toRank", "toFile"}
-            if expectedArguments != set(reqArgs):
-                fStr = "Invalid arguments, expected: {0}"
-                errorString = fStr.format(str(list(expectedArguments)))
-            else:
-                playerID = reqArgs["playerID"]
-                gameID = reqArgs["gameID"]
-                fromRank = reqArgs["fromRank"]
-                fromFile = reqArgs["fromFile"]
-                toRank = reqArgs["toRank"]
-                toFile = reqArgs["toFile"]
-                (successP, result) = self.__tournSys.makePly(
-                    playerID, gameID, fromRank, fromFile, toRank, toFile)
-                if successP:
-                    fStr = "SUCCESS \"{0}\""
-                    response = fStr.format() ## FIXME
-                else:
-                    errorString = result["error"]
-                    
+                    response = "SUCCESS \"{0}\"".format(str(result))
+                if not successP:
+                    errMsg = result["error"]
         else:
-            fStr = "Unrecognized verb \"{0}\" in request"
-            errorString = fStr.format(requestName)
-
-        ## Template for new request types
-        #elif requestName == "FIXME": ## FIXME
-        #    expectedArguments={"fixme"} ## FIXME
-        #    if expectedArguments != set(arguments):
-        #        fStr = "Invalid arguments, expected: {0}"
-        #        errorString = fStr.format(str(list(expectedArguments)))
-        #    else:
-        #        (errCode, result) = self.__tournSys.fixme() ## FIXME
-        #        if errCode == 0:
-        #            fStr = "SUCCESS \"{0}\""
-        #            response = fStr.format(str(playerID)) ## FIXME
-        #        elif errCode == -1:
-        #            errorString = "Unknown error"
-
-        if errorString == None:
-            # Provide the user with the response
-            self.sendLine(response)
+            errMsg = "Unrecognized verb \"{0}\" in request".format(requestName)
+        
+        # Respond to the client
+        if errMsg == None:
+            self.sendLine(response) # Provide client with the response
         else:
-            # Notify the user if they make an invalid request
-            fStr = "ERROR: {0} [query=\"{1}\"]"
-            errorMsg = fStr.format(errorString, line)
-            self.sendLine(errorMsg)
-            ## TODO: log invalid request
-            
+            response = "ERROR: {1} [query=\"{0}\"]".format(line, errMsg)
+            self.sendLine(response) # Provide client with the error
+        
+        # Close connection after each request
         self.transport.loseConnection()
         
 
