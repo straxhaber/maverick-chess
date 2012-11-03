c60f673d323a2dddb10d4c48f08b269d97145bcf
James Magnarelli <jmagnare@gmail.com>
2012-11-03 19:04:55 -0400

    filled out human.py code.  import issue still present

--- src/main/python/maverick/players/human.py
+++ src/main/python/maverick/players/human.py
@@ -22,6 +22,10 @@ from ..server import ChessMatch
 class HumanClient:
     ## TODO (James): Write comments for class and class methods
 
+    pieceCharMappings = {ChessBoard.ROOK: 'r', ChessBoard.KNGT: 'n',
+                         ChessBoard.BISH: 'b', ChessBoard.QUEN: 'q',
+                         ChessBoard.KING: 'k'}
+
     def __init__(self, host, port):
         self.nc = MaverickClient(host, port)
 
@@ -31,19 +35,19 @@ class HumanClient:
         self.playerID = self.nc.register(self.name)
 
         # Actually join a game
-        self.gameID = self.nc.joinGame(playerID)
+        self.gameID = self.nc.joinGame(self.playerID)
         print("You have joined game {0}".format(self.gameID))
 
         # Figure out what color player we are
-        self.state = self.nc.getState(gameID)
-        self.status = self.nc.getStatus(playerID, gameID)
-        self.myColor = status['youAreColor']
+        self.state = self.nc.getState(self.gameID)
+        self.status = self.nc.getStatus(self.playerID, self.gameID)
+        self.myColor = self.status['youAreColor']
         self.amWhite = self.myColor == ChessBoard.WHITE
         if self.myColor == ChessBoard.WHITE:
             self.myWinStatus = ChessMatch.STATUS_WHITE_WON
         else:
             self.myWinStatus = ChessMatch.STATUS_BLACK_WON
-    
+
     def playGame(self):
         # Wait for game to start
         while self.status == ChessMatch.STATUS_PENDING:
@@ -56,17 +60,16 @@ class HumanClient:
             sleep(1)  # Don't poll the server too rapidly
 
             newState = self.nc.getState(self.playerID, self.gameID)
-            
+
             # Prompt user for a move if it's their turn
             if newState['isWhitesTurn'] == self.amWhite:
                 self.getMoveAndMake()
-            
+
             # Print board and other info only if changes have occurred
             if newState['isWhitesTurn'] != self.state['isWhitesTurn']:
                 self.printBoard()
                 self.state = newState
-            
-            
+
             #Refresh status
             self.status = self.nc.getStatus(self.playerID, self.gameID)
 
@@ -82,43 +85,44 @@ class HumanClient:
             endGameStr.format("GAME CANCELLED")
 
         print endGameStr
-        
-    def printBoard(self, gameID):
-        '''
+
+    def getPieceChar(self, piece):
+        pieceType = piece[0]
+        pieceColor = piece[1]
+        pieceChar = HumanClient.pieceCharMappings[pieceType]
+
+        # Capitalize character if piece is white
+        if pieceColor == ChessBoard.WHITE:
+            pieceChar = pieceChar.upper()
+        else:
+            pieceChar = pieceChar.lower()
+
+        return pieceChar
+
+    def printBoard(self):
+        """
         #TODO: Fix code. Preliminary write by Brad, but I'm sure it is wrong.
         #TODO: Comment.
-    
-        '''
-    
-        self.gameID = gameID
-        print "1" + self.ChessBoard[0] + self.ChessBoard[1] + self.ChessBoard[2] +
-            self.ChessBoard[3] + self.ChessBoard[4] + self.ChessBoard[5] +
-            self.ChessBoard[6] + self.ChessBoard[7] + \n + "2" +
-            self.ChessBoard[8] + self.ChessBoard[9] + self.ChessBoard[10] +
-            self.ChessBoard[11] + self.ChessBoard[12] + self.ChessBoard[13] +
-            self.ChessBoard[14] + self.ChessBoard[15] + \n + "3" +
-            self.ChessBoard[16] + self.ChessBoard[17] + self.ChessBoard[18] +
-            self.ChessBoard[19] + self.ChessBoard[20] + self.ChessBoard[21] +
-            self.ChessBoard[22] + self.ChessBoard[23] + \n + "4" +
-            self.ChessBoard[24] + self.Chessboard[25] + self.ChessBoard[26] +
-            self.ChessBoard[27] + self.ChessBoard[28] + self.ChessBoard[29] +
-            self.ChessBoard[30] + self.ChessBoard[31] + \n + "5" +
-            self.ChessBoard[32] + self.ChessBoard[33] + self.ChessBoard[34] +
-            self.ChessBoard[35] + self.ChessBoard[36] + self.ChessBoard[37] +
-            self.ChessBoard[38] + self.ChessBoard[39] + \n + "6" +
-            self.ChessBoard[40] + self.ChessBoard[41] + self.ChessBoard[42] +
-            self.ChessBoard[43] + self.ChessBoard[44] + self.ChessBoard[45] +
-            self.ChessBoard[46] + self.ChessBoard[47] + \n + "7" +
-            self.ChessBoard[48] + self.ChessBoard[49] + self.ChessBoard[50] +
-            self.ChessBoard[51] + self.ChessBoard[52] + self.ChessBoard[53] +
-            self.ChessBoard[54] + self.ChessBoard[55] + \n + "8" +
-            self.ChessBoard[56] + self.ChessBoard[57] + self.ChessBoard[58] +
-            self.ChessBoard[59] + self.ChessBoard[60] + self.ChessBoard[61] +
-            self.ChessBoard[62] + self.ChessBoard[63] + \n + "1" + "2" + "3" +
-            "4" + "5" + "6" + "7" + "8"
+
+        """
+        fileOrdinal = ord('A')
+
+        for rank in range(1, ChessBoard.BOARD_SIZE + 1):
+            # Print rank number
+            print "{0}: ".format(rank)
+            row = self.board[rank]
+            for file in range(1, ChessBoard.BOARD_SIZE + 1):
+                piece = row[file]
+                print self.getPieceChar(piece)
+            print "\n"
+
+        # Print file numbers below board
+        for file in range(1, ChessBoard.BOARD_SIZE + 1):
+            print file
+        print "\n"
 
 
-def main(self, host='127.0.0.1', port=server.DEFAULT_MAVERICK_PORT):
+def main(self, host='127.0.0.1', port=7782):
         humanClient = HumanClient(host, port)
         humanClient.getNameAndJoin()
         humanClient.playGame()
