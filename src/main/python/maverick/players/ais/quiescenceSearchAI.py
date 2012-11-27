3fce3c45b43758c0639b5d416593dd15b0ee7fc1
James Magnarelli <jmagnare@gmail.com>
2012-11-27 15:25:56 -0500

    beginnings of alpha beta pruning based board search

--- src/main/python/maverick/players/ais/quiescenceSearchAI.py
+++ src/main/python/maverick/players/ais/quiescenceSearchAI.py
@@ -13,10 +13,10 @@ from argparse import ArgumentDefaultsHelpFormatter
 from argparse import ArgumentParser
 import logging
 
-from maverick.players.ais.common import MaverickAI
 from maverick.data import ChessBoard
 from maverick.players.ais.analyzers.likability import evaluateBoardLikability
-
+from maverick.players.ais.analyzers.stateExpansion import enumPossBoardMoves
+from maverick.players.ais.common import MaverickAI
 
 ## TODO (James): Make sure that heuristic functions to return an int in [-1..1]
 
@@ -36,7 +36,7 @@ class QLAI(MaverickAI):
     def getNextMove(self, board):
         """TODO PyDoc"""
 
-        SEARCH_DEPTH = 4
+        SEARCH_DEPTH = 2
 
         # Figure out our color
         if self.isWhite:
@@ -44,20 +44,21 @@ class QLAI(MaverickAI):
         else:
             color = ChessBoard.BLACK
 
-        nextMove = self.boardSearch(board, color, SEARCH_DEPTH, -1, 1)
+        (nextMove, _) = self._boardSearch(board, color, SEARCH_DEPTH, -1, 1,
+                                          True)
 
         (fromPosn, toPosn) = nextMove
 
         return (fromPosn, toPosn)
 
-    def boardSearch(self, board, color, depth, alpha, beta):
+    def _boardSearch(self, board, color, depth, min, max, isMaxNode):
         """Performs a search of the given board using alpha-beta pruning
 
         @param board: The starting board state to evaluate
         @param color: The color of the player to generate a move for
         @param depth: The number of plies forward that should be explored
-        @param alpha: Nodes with a likability below this will be ignored
-        @param beta: Nodes with a likability above this will be ignored
+        @param min: Nodes with a likability below this will be ignored
+        @param max: Nodes with a likability above this will be ignored
 
         @return: A tuple with the following elements:
                 1. None, or a move of the form (fromChessPosn, toChessPosn)
@@ -68,7 +69,11 @@ class QLAI(MaverickAI):
                     leaf node terminating the path
 
         Implementation based on information found here: http://bit.ly/t1dHKA"""
-        ##TODO (James): Write this
+        ##TODO (James): Incorporate quiescent search
+        ## TODO (James): Incorporate timeout
+        ## TODO (James): This gets stuck on depth == 0. Why?
+
+        self._logger.info("Searching for best move to depth {0}".format(depth))
 
         otherColor = ChessBoard.getOtherColor(color)
 
@@ -76,10 +81,53 @@ class QLAI(MaverickAI):
         if ((depth == 0) or
             board.isKingCheckmated(color) or
             board.isKingCheckmated(otherColor)):
-            return (None, evaluateBoardLikability(board))
+            return (None, evaluateBoardLikability(color, board))
         else:
-            moveChoices = self.enumPossBoardMoves(board, color)
-            pass
+            moveChoices = enumPossBoardMoves(board, color)
+
+            # Check whether seeking to find minimum or maximum value
+            if isMaxNode:
+                newMin = min
+                for move in moveChoices:
+                    nodeBoard = board.getResultOfPly(move[0], move[1])
+
+                    # Find the next move for this node, and how likable the
+                    # enemy will consider this child node
+                    (_, nodeEnemyLikability) = self._boardSearch(nodeBoard,
+                                                                otherColor,
+                                                                depth - 1,
+                                                                newMin, max,
+                                                                not isMaxNode)
+                    # Make note of the least likable branches that it still
+                    # makes sense to pursue, given how likable this one is
+                    if nodeEnemyLikability > newMin:
+                        newMin = nodeEnemyLikability
+
+                    # Don't search outside of the target range
+                    elif nodeEnemyLikability > max:
+                        return (move, max)
+                return (move, newMin)
+            else:
+                newMax = max
+                for move in moveChoices:
+                    nodeBoard = board.getResultOfPly(move[0], move[1])
+
+                    # Find how likable the enemy will consider this child node
+                    (_, nodeEnemyLikability) = self._boardSearch(nodeBoard,
+                                                                otherColor,
+                                                                depth - 1,
+                                                                min, newMax,
+                                                                not isMaxNode)
+
+                    # Make note of the most likable branches that it still
+                    # makes sense to pursue, given how likable this one is
+                    if nodeEnemyLikability < newMax:
+                        newMax = nodeEnemyLikability
+
+                    # Don't bother searching outside of our target range
+                    elif nodeEnemyLikability < min:
+                        return min
+                return (move, newMax)
 
 
 def runAI(host=None, port=None):
